036f70a96a1cb56d7c7a9e85f1c4a6c16496b5ab
opuzayed <zayedopu4933@gmail.com>
2022-08-17 15:37:49 +0600

    deposite calculation done

--- js/deposit.js
+++ js/deposit.js
@@ -5,6 +5,8 @@
 4.clear the deposit input field after getting the value
 5.get the previous deposit total
 6.calculate new deposit total and set the value to the deposit total
+7.Get current balance total
+8.calculate the new balance and set set to the balance total element
 */
 
 //step-1
@@ -28,4 +30,12 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     const newDepositTotal = previousDepositTotal + newDepositAmount;
     depositTotalElement.innerText = newDepositTotal;
 
+    //step-7
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotalAmount = parseFloat(previousBalanceTotalString);
+
+    //step-8
+    const newBalanceTotal = previousBalanceTotalAmount + newDepositAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
    })
