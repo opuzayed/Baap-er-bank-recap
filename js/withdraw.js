45e5d1a05178fad9c57fca92f16e2ad7df7512bb
opuzayed <zayedopu4933@gmail.com>
2022-08-17 16:30:50 +0600

    withdraw calculaten done

--- js/withdraw.js
+++ js/withdraw.js
@@ -4,6 +4,8 @@
 3.clear the withdraw input field
 4.get previous withdraw total
 5.calculate total withdraw amount and set it to the withdraw total element
+6.get previous balance
+7.calculate new balance and set it to the balance total element
 */
 
 //step-1
@@ -24,4 +26,14 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     //step-5
     const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
     withdrawTotalElement.innerText = newWithdrawTotal;
+
+    //step-6
+    const balanceElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    //step-7
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    console.log(newBalanceTotal);
+    balanceElement.innerText = newBalanceTotal;
 })
